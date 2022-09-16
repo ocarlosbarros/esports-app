fcf959b7a6ba4a6ad1bde57409b3417abcfeea7a
Carlos Barros <carlos_dbs@hotmail.com>
2022-09-15 23:09:37 -0300

    feat:creates components and use properties

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps){
+  return(
+    <TouchableOpacity>
+      <Text>
+        { props.title }
+      </Text>
+    </TouchableOpacity >
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello React Native</Text>
       <StatusBar style="auto"/>
+      <Button title='Send 1'/>
+      <Button title='Send 2'/>
+      <Button title='Send 3'/>
+      <Button title='Hello World'/>
     </View>
   );
 }
@@ -13,7 +31,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
