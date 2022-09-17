44c1c0df5c598938e1286689e738e8dcd54f3644
Carlos Barros <carlos_dbs@hotmail.com>
2022-09-17 18:28:25 -0300

    feat(App):adds new components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,28 @@
 import { StatusBar } from 'react-native';
 
 import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
 
-export default function App() {
+import { Loading } from './src/components/Loading';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+
+
+function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+  
   return (
     <Background>
     <StatusBar 
@@ -10,6 +30,9 @@ export default function App() {
       backgroundColor='transparent'
       translucent
     />
+    { fontsLoaded ? <Home /> : <Loading/> }
     </Background>
   );
 }
+
+export default App;
