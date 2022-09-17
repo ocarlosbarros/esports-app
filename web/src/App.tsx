b86991fb8b8aee8e7d9fd6a9f08a9a7df3ecfbac
Carlos Barros <carlos_dbs@hotmail.com>
2022-09-16 22:34:16 -0300

    refactor(App):removes codes of example

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,6 @@
-interface ButtonProps {
-  title:string
-}
-
-function Button(props: ButtonProps){
-  return (
-    <button>
-      { props.title }
-    </button>
-  )
-}
 function App() {
   return (
     <main>
-      <h1>Hello NLW!</h1>
-      <Button title="Send 1" />
-      <Button title="Send 2"/>
-      <Button title="Send 3"/>
-      <Button title="Hello World"/>
     </main>
   )
 }
