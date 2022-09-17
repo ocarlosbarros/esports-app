829aa985148f2de6c495a02e4983ebb921a78575
Carlos Barros <carlos_dbs@hotmail.com>
2022-09-17 00:13:20 -0300

    chore(csj-files):creates postcss.config and tailwind.config

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,10 +2,16 @@
 module.exports = {
   content: [
     "./src/**/*.tsx",
-    "index.html"
+    "./index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'galaxy':"url('./background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 33.08%, #43E7AD 74.57%, #E1D55D 3.94%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      }
+    },
   },
   plugins: [],
 }
