69b9d2558453351ab3c4d31be87cb800f4d72378
Carlos Barros <carlos_dbs@hotmail.com>
2022-09-17 13:18:22 -0300

    refactor(App):clear example code

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,38 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps){
-  return(
-    <TouchableOpacity>
-      <Text>
-        { props.title }
-      </Text>
-    </TouchableOpacity >
-  )
-}
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto"/>
-      <Button title='Send 1'/>
-      <Button title='Send 2'/>
-      <Button title='Send 3'/>
-      <Button title='Hello World'/>
+    <View>
+      
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
