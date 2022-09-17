92c1e3f84d0cd2bd9b13db56e1cca0beee244c67
Carlos Barros <carlos_dbs@hotmail.com>
2022-09-17 00:11:49 -0300

    feat(App):adds components and adds css to main tag

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,11 @@
+import Logo from "./components/Logo"
+import Carousel from './components/Carousel'
 function App() {
   return (
-    <main>
+    <main className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <Logo />
+      <h1 className="text-6xl text-white font-black mt-20">Seu <span className="text-transparent bg-nlw-gradient bg-clip-text" >duo</span> está aqui</h1>
+      <Carousel />
     </main>
   )
 }
