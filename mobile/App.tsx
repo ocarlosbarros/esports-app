e659dcbab4c432904b819a4d4ccf6f02032131a9
Carlos Barros <carlos_dbs@hotmail.com>
2022-09-17 15:48:11 -0300

    feat(App):adds new components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,15 @@
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View>
-      
-    </View>
+    <Background>
+    <StatusBar 
+      barStyle="light-content"
+      backgroundColor='transparent'
+      translucent
+    />
+    </Background>
   );
 }
