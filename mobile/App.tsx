95c2819d6ae81c13cfdf7e31cba2feb6a09d54de
Carlos Barros <carlos_dbs@hotmail.com>
2022-09-18 22:14:10 -0300

    feat(App):replace home to routes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'react-native';
 
 import { Background } from './src/components/Background';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 
 import { Loading } from './src/components/Loading';
 
@@ -30,7 +30,7 @@ function App() {
       backgroundColor='transparent'
       translucent
     />
-    { fontsLoaded ? <Home /> : <Loading/> }
+    { fontsLoaded ? <Routes /> : <Loading/> }
     </Background>
   );
 }
