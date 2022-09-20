b1995e7bb268f523f7d4d05e806446f91af4dd1b
Carlos Barros <carlos_dbs@hotmail.com>
2022-09-20 15:16:56 -0300

    feat(App):implements example to use notifications in the app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,8 @@
+import { useRef, useEffect } from 'react';
 import { StatusBar } from 'react-native';
-
-import { Background } from './src/components/Background';
-import { Routes } from './src/routes';
-
-import { Loading } from './src/components/Loading';
-
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
+import * as Notifications from 'expo-notifications';
+import { Subscription } from 'expo-modules-core';
 import {
   useFonts,
   Inter_400Regular,
@@ -13,16 +11,45 @@ import {
   Inter_900Black
 } from '@expo-google-fonts/inter';
 
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
+import './src/services/notificationConfigs';
 
 function App() {
-
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
     Inter_900Black
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
   
+  useEffect(() => {
+    getPushNotificationToken()
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      if(getNotificationListener.current && responseNotificationListener.current){
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  }, []);
+
+
+
   return (
     <Background>
     <StatusBar 
